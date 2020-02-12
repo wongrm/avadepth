ef6e4847865b1cc476e6ba0fe0a08eb98bd08edc
Mathieu Wong-Rose <mathieu.wong-rose@pwgsc-tpsgc.gc.ca>
2020-02-12 13:50:28 -0800

    AVD-8 jump to ISA results on tile selection

--- scripts/isa_func.js
+++ scripts/isa_func.js
@@ -54,7 +54,24 @@ if (!(typeof avaIFaceJS === 'undefined')) {
                 });
                     
                 avaIFaceJS.isa_func.tableReport.draw();
+                avaIFaceJS.sideNavPanel.reset();
+
+                var refMapString = (window.location.href.indexOf("fra") > -1) ? "Carte Physique" : "Reference Map";
+                var repHeaderString = (window.location.href.indexOf("fra") > -1) ? "Top of Report" : "Top of Report";
+                var sideNavTitleString = (window.location.href.indexOf("fra") > -1) ? "Navigate To" : "Navigate To";
+
+                avaIFaceJS.sideNavPanel.addTitle(sideNavTitleString);
+                avaIFaceJS.sideNavPanel.addLink(refMapString,"#ava_map_ttl");
+                avaIFaceJS.sideNavPanel.addLink(repHeaderString,"#reportTitleDiv");
+                avaIFaceJS.sideNavPanel.display();
+
                 avaIFaceJS.reportWindow.show();
+
+                if (avaIFaceJS.isa_func.tableReport){
+                    // (1) Place user page in the survey search results, as per client request - Last Updated 2018-09-28  
+                    var elemLocation = $("#reportTitleDiv").offset();
+                    window.scrollTo(elemLocation.left,elemLocation.top);
+                }
             });
         }
     };
